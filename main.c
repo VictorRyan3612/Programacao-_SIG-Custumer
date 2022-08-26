9b38bf27953aaea1d92b236eb86ba446f1748d55
VictorRyan3612 <106771198+VictorRyan3612@users.noreply.github.com>
2022-08-25 21:58:14 -0300

    Menu sobre

--- main.c
+++ main.c
@@ -20,3 +20,26 @@ int main() {
 
     return 0;
 }
+
+int menu_Sobre(){
+    setlocale (LC_ALL, "portuguese");
+
+    printf("\n"
+
+    "\t\tSobre o projeto:\n"
+    "\tSIG-Custumer: Um sistema de controle de cliente\n"
+    "As empresas da atualidade utilizam complexos sistemas gerenciais para, "
+    "entre várias outras funções, gerenciar e manter seus clientes, "
+    "além de ajudar a conseguir novos clientes. "
+    "O presente projeto tem como objetivo propor um software que implementa "
+    "funcionalidades básicas relacionadas ao controle de clientes de uma empresa, "
+    "facilitando a gestão\n"
+    "\n\n"
+
+    "\t\tDiscente/Autor do Projeto:\n"
+    "\tVictor Ryan Galvão Silva, matrícula:20220035163\n"
+    "\n"
+    );
+    getch(); // Aperte enter para continuar
+    return 0;
+}
