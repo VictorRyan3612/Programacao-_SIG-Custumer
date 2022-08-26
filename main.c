1d2a876dcb25cb5c62f7c38bd290865be487f41d
VictorRyan3612 <106771198+VictorRyan3612@users.noreply.github.com>
2022-08-25 21:01:06 -0300

    Criação do menu principal

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main() {
+    setlocale (LC_ALL, "portuguese");
+
+    printf("\n"
+    "\t\t==============================\n"
+    "\t\tPrograma Controle de Financias\n"
+    "\t\t==============================\n"
+    "\t\t1 - Módulo clientes\n"
+    "\t\t2 - Módulo em desenvolvimento\n"
+    "\t\t3 - Módulo em desenvolvimento\n"
+    "\t\t4 - Sobre e equipe\n"
+    "\t\t0 - Encerrar\n"
+    "\n"
+    );
+
+
+
+    return 0;
+}
