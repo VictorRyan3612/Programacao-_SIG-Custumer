cedfb7cb99bbc791d89bf5b1035e945ac56afcdf
VictorRyan3612 <106771198+VictorRyan3612@users.noreply.github.com>
2022-09-19 11:21:20 -0300

    create usuario.c

--- biblio.h
+++ biblio.h
@@ -1,5 +1,17 @@
+//in main.c ↓
+
 int menu_sobre(void);
 
 int menu_principal(void);
 
 int menu_usuario(void);
+
+// in usuario.c
+
+int modulo_usuario();
+
+int usuario_cadastro();
+int usuario_vizualizar();
+int usuario_pesquisar();
+int usuario_editar();
+int usuario_excluir();
---
+++ usuario.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+
+
+// Assinatura das funções:
+int menu_usuario();
+int usuario_cadastro();
+int usuario_vizualizar();
+int usuario_pesquisar();
+int usuario_editar();
+int usuario_excluir();
+
+
+
+
+int modulo_usuario(){
+    
+    setlocale (LC_ALL, "portuguese");
+    char opcao = '\0';
+
+    do{
+        menu_usuario();
+
+        printf("Digite uma opção:\n");
+        scanf("%c", &opcao);
+        getchar();
+
+        if (opcao != '0'){
+            if (opcao == '1'){
+                usuario_cadastro();
+            }
+            else if (opcao == '2'){
+                usuario_vizualizar();
+            }
+            else if (opcao == '3'){
+                usuario_pesquisar();
+            }
+            else if (opcao == '4'){
+                usuario_editar();
+            }
+            else if (opcao == '5'){
+                usuario_excluir();
+            }
+            
+            else {
+               printf("Opção não dessenvolvida ou inválida\n");
+            }
+            printf("Aperte enter para continuar\n");
+            getchar();
+        }
+            
+        
+        if (opcao == '0'){
+            printf("Voltando ao menu principal...\n");
+        }
+
+    }
+    while (opcao != '0');
+    return 0;
+}
+
+
+
+
+
+int usuario_cadastro(){
+    printf("Cadastro não disponível\n");
+
+    return 0;
+}
+int usuario_vizualizar(){
+    printf("vizualização não disponível\n");
+
+    return 0;
+}
+int usuario_pesquisar(){
+    printf("Busca não disponível\n");
+
+    return 0;
+}
+
+int usuario_editar(){
+    printf("Edição não disponível\n");
+
+    return 0;
+}
+int usuario_excluir(){
+    printf("Exclusão não disponível\n");
+
+    return 0;
+}
