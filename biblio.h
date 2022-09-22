57dcb734a1e1f47ac49b00d52c869f3f9ad35c91
VictorRyan3612 <106771198+VictorRyan3612@users.noreply.github.com>
2022-09-22 20:13:45 -0300

    Otimização em redeSocial.c

--- biblio.h
+++ biblio.h
@@ -24,12 +24,12 @@ int usuario_excluir(char a[], char b[], char c[]);
 
 // in redeSocial.c
 
-int modulo_redeSocial(void);
-int redeSocial_cadastro(void);
-int redeSocial_vizualizar(void);
-int redeSocial_pesquisar(void);
-int redeSocial_editar(void);
-int redeSocial_excluir(void);
+int modulo_redeSocial(char a[], char b[], char c[]);
+int redeSocial_cadastro(char a[], char b[], char c[]);
+int redeSocial_vizualizar(char a[], char b[], char c[]);
+int redeSocial_pesquisar(char a[], char b[], char c[]);
+int redeSocial_editar(char a[], char b[], char c[]);
+int redeSocial_excluir(char a[], char b[], char c[]);
 
 // in interesses.c
 
