272de607060607ab4652efff50701eef6177be8f
VictorRyan3612 <106771198+VictorRyan3612@users.noreply.github.com>
2022-09-26 23:22:39 -0300

    Menus principais melhorados

--- funcoes_menus.c
+++ funcoes_menus.c
@@ -31,18 +31,25 @@ int menu_sobre(void){
 
 int menu_principal(void){
     setlocale (LC_ALL, "portuguese");
-    printf("\n"
-    "\t\t==============================\n"
-    "\t\tPrograma Controle de Clientes\n"
-    "\t\t==============================\n"
-    "\t\t1 - Módulo Usuário\n"
-    "\t\t2 - Módulo Redes Sociais\n"
-    "\t\t3 - Módulo Interesses\n"
-    "\t\t4 - Sobre e equipe\n"
-    "\t\t0 - Encerrar\n"
-    "\n"
+    system("cls||clear");
+    printf(""
+        "|===================================================|\n"
+        "|===================================================|\n"
+        "|====                                           ====|\n"
+        "|====       Programa Controle de Clientes       ====|\n"
+        "|====                                           ====|\n"
+        "|====                                           ====|\n"
+        "|====            Digite qual deseja:            ====|\n"
+        "|====           1 - Módulo Usuário              ====|\n"
+        "|====           2 - Módulo Redes Sociais        ====|\n"
+        "|====           3 - Módulo Interesses           ====|\n"
+        "|====           4 - Sobre e equipe              ====|\n"
+        "|====           0 - Encerrar                    ====|\n"
+        "|====                                           ====|\n"
+        "|===================================================|\n"
+        "|===================================================|\n"
     );
-    
+
     return 0;
 }
 
@@ -50,17 +57,25 @@ int menu_usuario(void){
     setlocale (LC_ALL, "portuguese");
     system("cls||clear");
 
-        printf("\n"
-        "\t==============================\n"
-        "\tMenu Controle de Usuário\n"
-        "\t==============================\n"
-        "\t1 - Cadastrar usuário\n"
-        "\t2 - Exibir lista de usuários\n"
-        "\t3 - Procurar usuário\n"
-        "\t4 - Editar usuário\n"
-        "\t5 - Excluir usuário\n"
-        "\t0 - Sair\n"
-        );
+printf(""
+        "===================================================\n"
+        "===================================================\n"
+        "====                                           ====\n"
+        "====        Controle de conta de Usuário       ====\n"
+        "====                                           ====\n"
+        "====                                           ====\n"
+        "====          Digite qual deseja:              ====\n"
+        "====         1 - Cadastrar Usuário             ====\n"
+        "====         2 - Vizualizar Usuário            ====\n"
+        "====         3 - Procurar Usuário              ====\n"
+        "====         4 - Editar Usuário                ====\n"
+        "====         5 - Excluir Usuário               ====\n"
+        "====         0 - Voltar                        ====\n"
+        "====                                           ====\n"
+        "====                                           ====\n"
+        "===================================================\n"
+        "===================================================\n"
+    );
         return 0;
 }
 
@@ -68,17 +83,25 @@ int menu_redeSocial(void){
     setlocale (LC_ALL, "portuguese");
     system("cls||clear");
 
-        printf("\n"
-        "\t==============================\n"
-        "\tMenu Controle de rede social\n"
-        "\t==============================\n"
-        "\t1 - Cadastrar rede social\n"
-        "\t2 - Exibir lista de rede social\n"
-        "\t3 - Procurar rede social\n"
-        "\t4 - Editar rede social\n"
-        "\t5 - Excluir rede social\n"
-        "\t0 - Sair\n"
-        );
+printf(""
+        "===================================================\n"
+        "===================================================\n"
+        "====                                           ====\n"
+        "====        Controle das Redes Sociais         ====\n"
+        "====                                           ====\n"
+        "====                                           ====\n"
+        "====          Digite qual deseja:              ====\n"
+        "====         1 - Cadastrar Rede Social         ====\n"
+        "====         2 - Vizualizar Redes Sociais      ====\n"
+        "====         3 - Procurar Rede Social          ====\n"
+        "====         4 - Editar Rede Social            ====\n"
+        "====         5 - Excluir Rede Social           ====\n"
+        "====         0 - Voltar                        ====\n"
+        "====                                           ====\n"
+        "====                                           ====\n"
+        "===================================================\n"
+        "===================================================\n"
+    );
         return 0;
 }
 
@@ -86,17 +109,25 @@ int menu_interesse(void){
     setlocale (LC_ALL, "portuguese");
     system("cls||clear");
 
-        printf("\n"
-        "\t==============================\n"
-        "\tMenu Controle de Interesses\n"
-        "\t==============================\n"
-        "\t1 - Cadastrar Interesses\n"
-        "\t2 - Exibir lista de Interesses\n"
-        "\t3 - Procurar Interesse\n"
-        "\t4 - Editar Interesse\n"
-        "\t5 - Excluir Interesse\n"
-        "\t0 - Sair\n"
-        );
+    printf(""
+        "===================================================\n"
+        "===================================================\n"
+        "====                                           ====\n"
+        "====        Gerenciamento de  Interesse        ====\n"
+        "====                                           ====\n"
+        "====                                           ====\n"
+        "====          Digite qual deseja:              ====\n"
+        "====         1 - Cadastrar Interesse           ====\n"
+        "====         2 - Vizualizar Interesse          ====\n"
+        "====         3 - Procurar Interesse            ====\n"
+        "====         4 - Editar Interesse              ====\n"
+        "====         5 - Exlcuir Interesse             ====\n"
+        "====         0 - Voltar                        ====\n"
+        "====                                           ====\n"
+        "====                                           ====\n"
+        "===================================================\n"
+        "===================================================\n"
+    );
         return 0;
 }
 
@@ -216,6 +247,13 @@ void menu_redeSocial_excluir(void){
 
 
 ///// Usuário
+
+
+void menu_usuario_cadastro(void){
+
+
+
+}
 void menu_usuario_editar(void){
     setlocale (LC_ALL, "portuguese");
     system("cls||clear");
