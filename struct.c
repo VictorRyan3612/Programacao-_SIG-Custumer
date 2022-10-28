1e4f57e095c64de24b7be2ac0b8f7c98e8c011d1
VictorRyan3612 <106771198+VictorRyan3612@users.noreply.github.com>
2022-10-28 00:47:16 -0300

    Struct versão 3
    Com passagem de parâmetros

--- struct.c
+++ struct.c
@@ -7,27 +7,39 @@ struct usuario {
  char telefone[21];
 };
 
+void preencheUsuario(struct usuario *);
+void exibe_Usuario(struct usuario al);
+
 
 int main(void){
     system("cls||clear");
-
     struct usuario fulano;
+
+
+    preencheUsuario(&fulano);
+    exibe_Usuario(fulano);
+    
+    return 0;
+}
+
+
+void preencheUsuario(struct usuario* fulano) {
     printf("Programa Cadastro de usuarios\n\n");
     printf("Informe o nome do usuario: ");
-    scanf(" %80[^\n]", fulano.nome);
+    scanf(" %80[^\n]", fulano -> nome);
     printf("Informe o cpf do usuario: ");
-    scanf(" %50[^\n]", fulano.cpf);
+    scanf(" %50[^\n]", fulano -> cpf);
     printf("Informe o e-mail do usuario: ");
-    scanf(" %40[^\n]", fulano.email);
+    scanf(" %40[^\n]", fulano -> email);
     printf("Informe o telefone do usuario: ");
-    scanf(" %20[^\n]", fulano.telefone);
+    scanf(" %20[^\n]", fulano -> telefone);
 
+}
 
-
+void exibe_Usuario(struct usuario fulano){
     printf("\n= = = usuario Cadastrado = = =\n");
     printf("Nome do usuario: %s\n", fulano.nome);
     printf("cpf: %s\n", fulano.cpf);
     printf("Ender eletrônico: %s\n", fulano.email);
     printf("telefone usuario SIGAA: %s\n", fulano.telefone);
-    return 0;
 }
