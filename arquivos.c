7a0b4b35c5c911e9c89da0367ba45a7c3d3f0b93
VictorRyan3612 <106771198+VictorRyan3612@users.noreply.github.com>
2022-11-03 15:46:28 -0300

    arquivos parte 1

--- arquivos.c
+++ arquivos.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include "funcoes_aux.h"
 
 typedef struct usuario Usuario;
 
@@ -12,3 +13,52 @@ struct usuario {
 
 };
 
+void menuprincipal(void);
+int main(void);
+Usuario* preencheUsuario(void);
+void gravaUsuario(Usuario*);
+
+
+
+int main(void){
+    Usuario* fulano;
+    char op;
+
+    do{
+        menuprincipal();
+
+        printf("Digite uma opção:\n");
+        op = opcoes_pergunta();
+        
+        if (op == '1'){
+            fulano = preencheUsuario();
+            gravaUsuario(fulano);
+            free(fulano);
+        }
+        else if(op == '2'){
+            printf("\n");
+        }
+    }
+    while (op != '0');
+}
+
+void menuprincipal(){
+    printf(""
+    "Programa para uso de arquivo\n"
+    "1: Cadastra Usuario\n"
+    "2: Pesquisar Usuario\n"
+    );
+}
+void gravaUsuario(Usuario* fulano){
+    printf("\n");
+}
+
+Usuario* preencheUsuario(void){
+    Usuario* fulano;
+    fulano = (Usuario*) malloc(sizeof(Usuario));
+
+    printf("\n");
+
+    free(fulano);
+    return fulano;
+}
