e6a3c9464adcf15baeb91f585d293dafc987b7d9
VictorRyan3612 <106771198+VictorRyan3612@users.noreply.github.com>
2022-11-03 15:32:38 -0300

    arquivos parte 0

---
+++ arquivos.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct usuario Usuario;
+
+struct usuario {
+    char nome[81];
+    char cpf[51];
+    char email[41];
+    char telefone[21];
+
+};
+
