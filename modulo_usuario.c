75e63163f039759f99996254438b58775bd2a018
VictorRyan3612 <106771198+VictorRyan3612@users.noreply.github.com>
2022-11-07 14:53:35 -0300

    Ignorar funcoes editar e exluir de usuario.c

--- modulo_usuario.c
+++ modulo_usuario.c
@@ -202,127 +202,131 @@ int usuario_pesquisar(const Usuario* fulano){
 }
 
 int usuario_editar(Usuario* fulano){
-    char opcao;
-    int resp;
+    printf("\n");
+
+    // char opcao;
+    // int resp;
     
-    menu_usuario_editar();
+    // menu_usuario_editar();
 
-    printf("\n\n");
-    printf("Suas informações atualmente cadastradas são:\n");
-    usuario_vizualizar(fulano);
+    // printf("\n\n");
+    // printf("Suas informações atualmente cadastradas são:\n");
+    // usuario_vizualizar(fulano);
 
-    opcao = opcoes_pergunta();
+    // opcao = opcoes_pergunta();
 
-    // Editar nome
-    if (opcao == '1'){
-        printf("Seu atual nome é esse:\n");
-        printf("%s", fulano -> nome);
-        printf("\n\n");
-        do{
-            printf(""
-                "=======================================\n"
-                "====      Informe o atualizado:    ====\n"
-                "=======================================\n"
-            );
-            printf("\n");
-            scanf("%20[^\n]", fulano -> nome);
-            getchar();
+    // // Editar nome
+    // if (opcao == '1'){
+    //     printf("Seu atual nome é esse:\n");
+    //     printf("%s", fulano -> nome);
+    //     printf("\n\n");
+    //     do{
+    //         printf(""
+    //             "=======================================\n"
+    //             "====      Informe o atualizado:    ====\n"
+    //             "=======================================\n"
+    //         );
+    //         printf("\n");
+    //         scanf("%20[^\n]", fulano -> nome);
+    //         getchar();
 
-            resp = validar_nome(fulano -> nome);
-            if (resp != True){
-                printf("Caractere inválido detectado, Digite novamente:\n");
-            }
-        } while (resp != True);
-    }
-    // Editar email
-    else if (opcao == '2'){
-        printf("Seu atual email é esse:\n");
-        printf("%s", fulano -> email);
-        printf("\n\n");
-        do{
-            printf(""
-                "=======================================\n"
-                "====      Informe o atualizado:    ====\n"
-                "=======================================\n"
-            );
-            printf("\n");
-            scanf("%s", fulano -> email);
-            getchar();
+    //         resp = validar_nome(fulano -> nome);
+    //         if (resp != True){
+    //             printf("Caractere inválido detectado, Digite novamente:\n");
+    //         }
+    //     } while (resp != True);
+    // }
+    // // Editar email
+    // else if (opcao == '2'){
+    //     printf("Seu atual email é esse:\n");
+    //     printf("%s", fulano -> email);
+    //     printf("\n\n");
+    //     do{
+    //         printf(""
+    //             "=======================================\n"
+    //             "====      Informe o atualizado:    ====\n"
+    //             "=======================================\n"
+    //         );
+    //         printf("\n");
+    //         scanf("%s", fulano -> email);
+    //         getchar();
             
-            resp = validar_email(fulano -> email);
-            if (resp != True){
-                printf("Caractere inválido detectado, Digite novamente:\n");
-            }
-        } while (resp != True);
-    }
+    //         resp = validar_email(fulano -> email);
+    //         if (resp != True){
+    //             printf("Caractere inválido detectado, Digite novamente:\n");
+    //         }
+    //     } while (resp != True);
+    // }
 
         
-    // Editar telefone
-    else if (opcao == '3'){
-        printf("Seu atual telefone é esse:\n");
-        printf("%s", fulano -> telefone);
-        printf("\n\n");
-        do{
-            printf(""
-                "=======================================\n"
-                "====      Informe o atualizado:    ====\n"
-                "=======================================\n"
-            );
-            printf("\n");
-            scanf("%s", fulano -> telefone);
-            getchar();
+    // // Editar telefone
+    // else if (opcao == '3'){
+    //     printf("Seu atual telefone é esse:\n");
+    //     printf("%s", fulano -> telefone);
+    //     printf("\n\n");
+    //     do{
+    //         printf(""
+    //             "=======================================\n"
+    //             "====      Informe o atualizado:    ====\n"
+    //             "=======================================\n"
+    //         );
+    //         printf("\n");
+    //         scanf("%s", fulano -> telefone);
+    //         getchar();
 
-            resp = validar_telefone(fulano -> telefone);
-            if (resp != True){
-                printf("Caractere inválido detectado, Digite novamente:\n");
-            }
-        } while (resp != True);
-    }
+    //         resp = validar_telefone(fulano -> telefone);
+    //         if (resp != True){
+    //             printf("Caractere inválido detectado, Digite novamente:\n");
+    //         }
+    //     } while (resp != True);
+    // }
 
     return 0;
 }
 
 int usuario_excluir(Usuario* fulano){
-    char opcao;
-    int resp;
+    printf("\n");
 
-    menu_usuario_excluir();
+    // char opcao;
+    // int resp;
 
-    printf("\n\n");
-    printf("Suas informações atualmente cadastradas são:\n");
-    usuario_vizualizar(fulano);
+    // menu_usuario_excluir();
 
-    printf("Digite qual deseja:\n");
-    opcao = opcoes_pergunta();
+    // printf("\n\n");
+    // printf("Suas informações atualmente cadastradas são:\n");
+    // usuario_vizualizar(fulano);
 
-    resp = confirmacao();
-    if (resp == True){
-        if (opcao == '1'){
-            printf("Exluindo seu nome\n");
-            strcpy(fulano -> nome, "");
-            printf("Exluído com sucesso\n");
+    // printf("Digite qual deseja:\n");
+    // opcao = opcoes_pergunta();
 
-        }
+    // resp = confirmacao();
+    // if (resp == True){
+    //     if (opcao == '1'){
+    //         printf("Exluindo seu nome\n");
+    //         strcpy(fulano -> nome, "");
+    //         printf("Exluído com sucesso\n");
 
-        else if (opcao == '2'){
-            printf("Exluindo seu cpf cadastrado\n");
-            strcpy(fulano -> cpf, "");
-            printf("Exluído com sucesso\n");
+    //     }
 
-        }
+    //     else if (opcao == '2'){
+    //         printf("Exluindo seu cpf cadastrado\n");
+    //         strcpy(fulano -> cpf, "");
+    //         printf("Exluído com sucesso\n");
+
+    //     }
         
-        else if (opcao == '3'){
-            printf("Exluindo seu email cadastrado\n");
-            strcpy(fulano -> email, "");
-            printf("Exluído com sucesso\n");
+    //     else if (opcao == '3'){
+    //         printf("Exluindo seu email cadastrado\n");
+    //         strcpy(fulano -> email, "");
+    //         printf("Exluído com sucesso\n");
             
-        }
-    }
-        else if (opcao == '4'){
-            printf("Exluindo seu telefone cadastrado\n");
-            strcpy(fulano -> telefone, "");
-            printf("Exluído com sucesso\n");
+    //     }
+    // }
+    //     else if (opcao == '4'){
+    //         printf("Exluindo seu telefone cadastrado\n");
+    //         strcpy(fulano -> telefone, "");
+    //         printf("Exluído com sucesso\n");
             
-        }
+    //     }
     return 0;
 }
