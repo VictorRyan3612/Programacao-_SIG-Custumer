8788dacfd7c939de909761611b73123aa3aacfaa
VictorRyan3612 <106771198+VictorRyan3612@users.noreply.github.com>
2022-11-17 15:47:19 -0300

    Abrir arquivo

--- dinamicas.c
+++ dinamicas.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-
-
+#include <string.h>
+#include "funcoes_aux.h"
 
 void estru_dinam(void){
     printf("\n");
-}
+    FILE* fp;
+    fp = fopen("titanic.csv","rt");
+
+    
+
+
+
+
+
+    fclose(fp);
+    }
+
+
