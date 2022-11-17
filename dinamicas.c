347bef4ff519a4b081ea40e4aeb0947f78568d42
VictorRyan3612 <106771198+VictorRyan3612@users.noreply.github.com>
2022-11-17 16:08:12 -0300

    Struct Com campos do titanic.csv
    + arquivo cvs do relatorio titanic, adquirido a partir de https://github.com/datasciencedojo/datasets/blob/master/titanic.csv

--- dinamicas.c
+++ dinamicas.c
@@ -3,12 +3,30 @@
 #include <string.h>
 #include "funcoes_aux.h"
 
+struct titanic{
+    int pass_id;
+    int sobre;
+    int classe;
+    char nome[80];
+    char sex[7];
+    int idade;
+    char ipmarket;
+    int q_irmaos;
+    int q_fami;
+    char ticket[20];
+    int tarifa;
+    char cabine[10];
+    char embarque; 
+    // (C = Cherbourg, Q = Queenstown, S = Southampton)
+};
+
+
 void estru_dinam(void){
     printf("\n");
     FILE* fp;
     fp = fopen("titanic.csv","rt");
 
-    
+
 
 
 
