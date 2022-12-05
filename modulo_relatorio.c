eac61c128ab41311279650fe055316c11d787c79
VictorRyan3612 <106771198+VictorRyan3612@users.noreply.github.com>
2022-12-05 19:37:54 -0300

    Update modulo_relatorio.c

--- modulo_relatorio.c
+++ modulo_relatorio.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
 
+#include "funcoes_aux.h"
+#include "modulo_usuario.h"
+#include "modulo_redeSocial.h"
+#include "modulo_midia.h"
+
 
 void modulo_relatorio(){
     printf("\n");
